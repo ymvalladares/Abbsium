86389c7e3b0a2e8e2469ef42eb402d69e194e5df
Yordan <yordan.j.martinez@gmail.com>
2025-07-06 11:34:21 -0400

    Add Wake up fuction

---
+++ server/Server/Controllers/HealthApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    public class HealthApiController : Base_Control_Api
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "Ok", timestamp = DateTime.UtcNow});
+        }
+    }
+}
