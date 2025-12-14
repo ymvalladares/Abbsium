d86297d1acc6e717c72c67f37dd21d16ef852370
Yordan <yordan.j.martinez@gmail.com>
2025-12-14 10:45:33 -0500

    Fixing bugs

--- server/Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
+++ server/Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ee92f0df8ca0ea95441477554aa8cfb0b29f8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ae351090254d0961e95fb0eefcac517faa1a37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
